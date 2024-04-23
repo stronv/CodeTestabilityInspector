d64b1e58e9318fcfcd4aeee6d15c5b1d37648a40
Artem Tabachenko <fexo5467@gmail.com>
2024-04-23 17:49:31 +0300

    Initial Commit

---
+++ CodeTestabilityInspector/CodeTestabilityInspector.h
@@ -0,0 +1,18 @@
+//
+//  CodeTestabilityInspector.h
+//  CodeTestabilityInspector
+//
+//  Created by Artyom Tabachenko on 23.04.2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CodeTestabilityInspector.
+FOUNDATION_EXPORT double CodeTestabilityInspectorVersionNumber;
+
+//! Project version string for CodeTestabilityInspector.
+FOUNDATION_EXPORT const unsigned char CodeTestabilityInspectorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CodeTestabilityInspector/PublicHeader.h>
+
+
